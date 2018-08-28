c7411017dfb50cde33ba3e64b6c69ba6b508e886
github username <github email address>
2018-08-28 15:01:47 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,21 @@
 // your Bomb code here!
+
+import React from 'react';
+
+export default class Bomb extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      secondsLeft: this.props.initialCount
+    }
+  }
+
+  render(){
+    console.log(this.state.secondsLeft)
+    return (
+      <div>
+      {(this.state.secondsLeft > 0) ? `${this.state.secondsLeft} seconds left before I go boom!` : 'Boom!'}
+      </div>
+    )
+  }
+}
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,23 @@
-// your ImageSlider code here!
+import React from 'react';
+
+class ImageSlider extends React.Component {
+
+  constructor(){
+    super()
+    this.state = {
+      currentSlideIndex: 0
+    }
+  }
+
+  render(){
+    console.log('Hello World')
+    return(
+      <div>
+      I am on slide {this.state.currentSlideIndex}
+      </div>
+    )
+  }
+
+}
+
+export default ImageSlider
